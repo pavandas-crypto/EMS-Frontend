e736a3f14ccf2c1e9abffb49a7a0f15cbd1d6080
Pratiksha Ghosh <pra3tughosh@gmail.com>
2026-04-28 15:45:29 +0530

    errorfree

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./component/login.jsx";
-<<<<<<< HEAD
 import AdminLayout from "./component/Admin/AdminLayout.jsx";
 import AdminDashboard from "./component/Admin/AdminDashboard.jsx";
 import EventCreate from "./component/Admin/EventCreate.jsx";
@@ -10,21 +9,9 @@ import EventForm from "./component/Admin/EventForm.jsx";
 import Registrations from "./component/Admin/Registrations.jsx";
 import GenerateTickets from "./component/Admin/GenerateTickets.jsx";
 import Verifiers from "./component/Admin/Verifiers.jsx";
-=======
 import LandingPage from "./component/eventpage/landingpage.jsx";
 import RegisterForm from "./component/eventpage/registerform.jsx";
-import VerifierApp from "./component/verifier/VerifierApp.jsx";
-import AdminLayout from "./component/admin/AdminLayout.jsx";
-import AdminDashboard from "./component/admin/AdminDashboard.jsx";
-import EventCreate from "./component/admin/EventCreate.jsx";
-import EventEdit from "./component/admin/EventEdit.jsx";
-import EventManage from "./component/admin/EventManage.jsx";
-import EventForm from "./component/admin/EventForm.jsx";
-import Registrations from "./component/admin/Registrations.jsx";
-import GenerateTickets from "./component/admin/GenerateTickets.jsx";
-import Verifiers from "./component/admin/Verifiers.jsx";
->>>>>>> 801f76918e360e13599da7aa11f8a09cfe6a04fd
-
+import VerifierApp from "./component/verifier/verifierapp.jsx";  
 // Protected Route Component
 function ProtectedRoute({ children }) {
   const isAuthenticated = localStorage.getItem("adminToken") === "admin_logged_in";
